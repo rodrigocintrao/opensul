66bb8e10bff0c7fba8b60c7fbf8a8643e50dc2d5
Rodrigo Cintrao <rodrigo.cintrao@gmail.com>
2018-08-30 22:21:41 -0300

    footer

--- wp-content/themes/opensul/index.php
+++ wp-content/themes/opensul/index.php
@@ -13,6 +13,7 @@
     <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
     <script src="<?php bloginfo('template_url'); ?>/js/slick.min.js"></script>
     <script src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
+    <?php wp_head(); ?>
 </head>
 <body>
     <header class="header">
@@ -267,5 +268,38 @@
             </div>
         </section>
     </main>
+    <footer class="footer">
+        <section class="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3453.616517266903!2d-51.220774885281614!3d-30.047857781881!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95197859d2e8b319%3A0xc4238500125bed13!2sAv.+Ipiranga%2C+1000+-+Menino+Deus%2C+Porto+Alegre+-+RS%2C+90160-090!5e0!3m2!1spt-BR!2sbr!4v1535675236599" frameborder="0" style="border:0" allowfullscreen></iframe>	
+        </section>
+        <section class="contact p-tb-lg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4 text-center p-tb-sm">
+                        <div class="contact-box">
+                            <i class="fas fa-phone fa-3x"></i>
+                            <p class="no-margin">Telefone Fixo:</p>
+                            <h3 class="no-margin">(51) 3049 9898</h3>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 text-center p-tb-sm">
+                        <div class="contact-box">
+                            <i class="fas fa-mobile-alt fa-3x"></i>
+                            <p class="no-margin">Celular/Whatsapp:</p>
+                            <h3 class="no-margin">(51) 98988 9898</h3>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 text-center p-tb-sm">
+                        <div class="contact-box">
+                            <i class="fab fa-skype fa-3x"></i>
+                            <p class="no-margin">Skype:</p>
+                            <h3 class="no-margin">opensul</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </footer>
 </body>
+<?php wp_footer(); ?>
 </html>
--- wp-content/themes/opensul/js/main.js
+++ wp-content/themes/opensul/js/main.js
@@ -1,4 +1,5 @@
 $(function(){
+
     $(".slider").slick();
 
     $(".slider-represented").slick({
